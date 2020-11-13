f930ec88dd1fdeb582b039f1c621273b07ddc0db
Qiuli Han <jeef@Qiulis-iPad.home.local>
2020-11-13 13:57:55 -0500

    initial commit with player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10f;
+    Rigidbody2D rb;
+
+    float mx;
+    float my;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        my = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(mx, my) * speed;
+    }
+}
