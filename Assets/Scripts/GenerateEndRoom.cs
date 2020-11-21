e65596375ee76accc1ef5f0844ef29bb42a3240d
Kjumar <jganguli@hotmail.ca>
2020-11-20 21:19:24 -0500

    EndRoom Generation with some prfab rooms

---
+++ Assets/Scripts/GenerateEndRoom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEndRoom : MonoBehaviour
+{   
+    Transform tf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+
+        GameObject cafeteriaPrefab = Resources.Load<GameObject>("CafeteriaRoom");
+        GameObject serverPrefab = Resources.Load<GameObject>("ServerRoom");
+        GameObject storagePrefab = Resources.Load <GameObject>("StorageRoom");
+
+        int i = Random.Range(1, 4);
+
+        if (i == 1)
+        {
+            GameObject room = (GameObject)Instantiate(cafeteriaPrefab, tf);
+        }
+        else if (i == 2)
+        {
+            GameObject room = (GameObject)Instantiate(serverPrefab, tf);
+        }
+        else if (i == 3)
+        {
+            GameObject room = (GameObject)Instantiate(storagePrefab, tf);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
